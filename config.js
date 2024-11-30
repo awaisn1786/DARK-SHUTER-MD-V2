77eab51021565bfc344d8fd03a6074bc8bc24af1
Anrar <maharabrar14965@gmail.com>
2024-11-30 20:44:40 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: 'DARK-SHUTER-MD=jBlygBJa#GJc8Y6Gz_O_Qk7eRkUpHwPguf5ql_nu88fF1cSvFfWA',
-GITHUB_TOKEN: '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',
+GITHUB_TOKEN: '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',
 REPO_NAME: 'put like repo name',
 GITHUB_USER_NAME: 'put your github user name',
 
